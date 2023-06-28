c5291808358eac7cc2fe1040fd9dfa52dc06796b
alecasentini <ale.casentini@gmail.com>
2023-06-28 13:40:04 +0200

    Primo push - Backend

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.dashboard')
+
+@section('title')
+Portfolio | Project Show
+@endsection
+
+@section('content')
+<h1>Singolo Projects: {{$project->name}}</h1>
+
+<img class="img-fluid" src="{{ asset('storage/' . $project->cover_image) }}" alt="">
+
+<p>
+    Descrizione: {{$project->description}}
+</p>
+<p>
+    Cliente: {{$project->client}}
+</p>
+
+
+{{-- questa dovrebbe essere la funzione corretta {{$project->type->name}} --}}
+
+<p>
+    Tipo: {{ $type->name }}
+    
+</p>
+
+<p>
+    Tecnologie:
+    @if ($project->technologies)
+        @foreach ($project->technologies as $elem)
+            <span>{{ $elem->name }}</span>
+        @endforeach
+    @endif
+</p>
+
+
+
+
+@endsection
