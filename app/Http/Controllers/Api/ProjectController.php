90c07d58b248d0a8e2e6f91a99eabf6c042ec5db
alecasentini <ale.casentini@gmail.com>
2023-06-28 13:57:24 +0200

    Aggiunto Api/ProjectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Admin\Project;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+
+    public function index()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+}
