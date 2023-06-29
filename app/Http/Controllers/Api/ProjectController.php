72ba372a4183e52b0aa6571b3915fb5fa0ac20cb
alecasentini <ale.casentini@gmail.com>
2023-06-29 15:58:43 +0200

    Aggiunto show al controller e api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -21,4 +21,19 @@ class ProjectController extends Controller
             'projects' => $projects
         ]);
     }
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Project non trovato'
+            ]);
+        }
+    }
 }
